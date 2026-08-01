9a55a69fbc80f2397ce44760ed2c60968579dd52
MarlevionWinston <winstonmarlevion@gmail.com>
2026-08-01 04:20:13 -0700

    Add files via upload

---
+++ Slideshow.js
@@ -0,0 +1,47 @@
+let arrImages = [
+            "./images/alexander-kaufmann-qRBEPQeJ6hE-unsplash.jpg",
+            "./images/kirill-chukhnov-c5adA6zENBg-unsplash.jpg",
+            "./images/kirill-chukhnov-PEXAymTSZdE-unsplash.jpg",
+            "./images/maverick-frame-4-yLQog02Tc-unsplash.jpg",
+        ];
+
+        // variable to keep track of the current image index
+        let currImage = 0;
+
+        // show slide - pass in the index of the image to show
+        function showSlide(index){
+            // get a shortcut/nickname for the img element in HTML
+            let image = document.getElementById("imgSlide");
+            // change the image source to the relative address from the array
+            image.src = arrImages[index];
+        }
+
+        // previous button function click event
+        function prevSlide(){
+            // when the user clicks the previous button, change currImage - 1
+            currImage--;
+
+            // if the user at the first image in the array, and they clicked the Previous button let's show the last image in the array, to give a "loop effect"
+            if(currImage < 0){
+                // change currImage from -1 to the last index of the array
+                currImage = arrImages.length - 1;
+            }
+
+            // load the new image
+            showSlide(currImage);
+        }
+
+        // next button function click event
+        function nextSlide(){
+            // when the user clicks the Previous button, change currImage + 1
+            currImage++;
+
+            // if currImage has exceeded the number of images in the array, then rest back to the first image in the array, to give a "loop effect"
+            if(currImage >= arrImages.length){
+                currImage = 0;
+            }
+
+             // load the new image
+            showSlide(currImage);
+
+        }  
